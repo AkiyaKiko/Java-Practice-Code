17ab6266a3580a09f63f603391de5ecaba3c586b
AkiyaKiko <sinchialveno@protonmail.com>
2024-05-01 11:39:07 +0800

    new code

---
+++ MISC/test.java
@@ -0,0 +1,14 @@
+package MISC;
+
+/**
+ * test
+ */
+public class test {
+
+    public static void main(String[] args) {
+        int x = 0;
+        for (int i=0;i<5;i++)
+        x += i;
+        System.out.println(x);
+    }
+}
